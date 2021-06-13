fc152284d57ead25fe7dc41af072075f211fa59c
maheravi <56476428+maheravi@users.noreply.github.com>
2021-06-13 13:03:55 +0430

    Update A21-1.py

--- A21-1.py
+++ A21-1.py
@@ -2,32 +2,25 @@ import cv2
 import numpy as np
 
 
-def Raster(rows):
-    image = np.zeros((rows, rows))
+blockSize = 75
+imageSize = blockSize * 8
+chessBoard = np.zeros((imageSize, imageSize))
 
-    for i in range(0, rows, 5):
-        if i % 2 == 0:
-            for j in range(0, rows, 5):
-                if j % 2 == 0:
-                    image[i:i+5, j:j+5] = 0
-                else:
-                    image[i:i+5, j:j+5] = 255
-        else:
-            for j in range(0, rows, 5):
-                if j % 2 == 0:
-                    image[i:i+5, j:j+5] = 255
-                else:
-                    image[i:i+5, j:j+5] = 0
+for i in range(0, imageSize, blockSize):
+    if i % 2 == 0:
+        for j in range(0, imageSize, blockSize):
+            if j % 2 == 0:
+                chessBoard[i:imageSize, j:imageSize] = 0
+            else:
+                chessBoard[i:imageSize, j:imageSize] = 255
 
-    cv2.imshow('my pic 1', image)
-    cv2.waitKey()
+    else:
+        for j in range(0, imageSize, blockSize):
+            if j % 2 == 0:
+                chessBoard[i:imageSize, j:imageSize] = 255
+            else:
+                chessBoard[i:imageSize, j:imageSize] = 0
 
 
-size = int(input("Please size : "))
-Raster(size)
-
-board = np.zeros((size, size))
-board[::2, 1::2] = 0
-board[1::2, ::2] = 255
-cv2.imshow('my pic 2', board)
+cv2.imshow("Chess board", chessBoard)
 cv2.waitKey()
